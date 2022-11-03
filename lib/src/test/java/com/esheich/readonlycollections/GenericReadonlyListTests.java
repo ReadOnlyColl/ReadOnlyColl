64bd09cb800260b7f478968cfaf30fab1938b690
상현 김 <thwj@me.com>
2022-11-03 23:34:38 +0900

    sdf

--- lib/src/test/java/com/esheich/readonlycollections/GenericReadonlyListTests.java
+++ lib/src/test/java/com/esheich/readonlycollections/GenericReadonlyListTests.java
@@ -1,6 +1,7 @@
 package com.esheich.readonlycollections;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import java.util.ArrayList;
 import org.junit.jupiter.api.Test;
 
@@ -25,5 +26,25 @@ public class GenericReadonlyListTests {
 
         // Assert
         assertThat(actualValue).isEqualTo(expectedValue);
+    }
+
+    @Test
+    void can_not_get_a_value_if_out_of_range() {
+
+        // Arrange
+        var source = new ArrayList<String>();
+        source.add("a");
+        source.add("b");
+
+        int outOfRange = source.size() + 1;
+
+        var sut = new GenericReadonlyList<String>(source);
+
+        // Act
+        Runnable getting = () -> sut.get(outOfRange);
+
+        // Assert
+        assertThatThrownBy(getting::run)
+            .isExactlyInstanceOf(IndexOutOfBoundsException.class);
     }
 }
