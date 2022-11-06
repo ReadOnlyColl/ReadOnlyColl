85ad3fa92ddd6276d33eeb9bd5d4d377bc64705a
상현 김 <thwj@me.com>
2022-11-06 20:24:35 +0900

    Test를 위한 도구를 작성합니다.

---
+++ lib/src/test/java/com/esheich/readonlycollections/testtool/StringArrayConverter.java
@@ -0,0 +1,27 @@
+package com.esheich.readonlycollections.testtool;
+
+import org.junit.jupiter.params.converter.ArgumentConversionException;
+import org.junit.jupiter.params.converter.SimpleArgumentConverter;
+
+public class StringArrayConverter extends SimpleArgumentConverter {
+
+    @Override
+    protected Object convert(
+        Object source, Class<?> targetType)
+        throws ArgumentConversionException {
+
+        if (source instanceof String
+            && String[].class.isAssignableFrom(targetType)) {
+
+            return ((String) source).split("\\s*,\\s*");
+        } else if (source == null) {
+            return new String[0];
+        } else {
+
+            String message = "Conversion from " + source.getClass() + " to "
+                                               + targetType + " not supported.";
+
+            throw new IllegalArgumentException(message);
+        }
+    }
+}
