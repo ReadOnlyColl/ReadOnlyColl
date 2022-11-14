495786b223790acfb1516aaf96c8638f437f8a8e
ahbyeol <ahbyeol.kim@onepredict.com>
2022-11-14 10:42:47 +0900

    수정합니다.

--- readonlycoll/src/main/java/readonlycollections/GeneralReadOnlySet.java
+++ readonlycoll/src/main/java/readonlycollections/GeneralReadOnlySet.java
@@ -1,14 +1,10 @@
 package readonlycollections;
 
 import java.util.Collections;
-import java.util.Iterator;
 import java.util.Set;
-import java.util.stream.Stream;
 
 public class GeneralReadOnlySet<T>
     extends GeneralReadOnlyCollection<T> implements ReadOnlySet<T> {
-
-    //private final Set<T> source;
 
     public GeneralReadOnlySet(Set<T> source) {
 
@@ -16,46 +12,10 @@ public class GeneralReadOnlySet<T>
     }
 
     @Override
-    public int size() {
-
-        return this.source.size();
-    }
-
-    @Override
-    public boolean isEmpty() {
-
-        return this.source.isEmpty();
-    }
-
-    @Override
-    public boolean contains(T value) {
-
-        return this.source.contains(value);
-    }
-
-    @Override
     public Set<T> asRuntimeReadOnly() {
 
         Set<T> source = getSourceAsSet();
         return Collections.unmodifiableSet(source);
-    }
-
-    @Override
-    public Iterator<T> iterator() {
-
-        return this.source.iterator();
-    }
-
-    @Override
-    public Stream<T> stream() {
-
-        return this.source.stream();
-    }
-
-    @Override
-    public Stream<T> parallelStream() {
-
-        return this.source.parallelStream();
     }
 
     protected Set<T> getSourceAsSet() {
